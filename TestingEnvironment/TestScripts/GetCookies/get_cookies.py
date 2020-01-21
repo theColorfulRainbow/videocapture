87134371e2d90408d7f901fb689b7ae9c05aaa06
theColorfulRainbow <30596510+theColorfulRainbow@users.noreply.github.com>
2020-01-21 14:48:35 +0000

    more cookies

--- TestingEnvironment/TestScripts/GetCookies/get_cookies.py
+++ TestingEnvironment/TestScripts/GetCookies/get_cookies.py
@@ -5,6 +5,8 @@ Script made as a method that can be called
 
 import time
 import pickle
+import requests
+import http.cookiejar as cookielib
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
@@ -12,51 +14,76 @@ URL = 'https://echo360.org.uk/directLogin'
 EMAIL = 's1628465@ed.ac.uk'
 PASSWORD = 'anj3la.1.yoga'
 
-
+def parseCookieFile(cookies):
+    cookies = {}
+    for cookie in cookies:
+        if not re.match(r'^\#', line):
+            lineFields = line.strip().split('\t')
+            cookies[lineFields[5]] = cookie[6]
+    return cookies
 
 def get_cookies(url=URL,email=None,password=None):
-	if(email==None):
-		email = EMAIL
-		print('email = {}'.format(EMAIL))
-	if(password==None):
-		password = PASSWORD
-		print('password = {}'.format(PASSWORD))
-	options = Options()
-	# not graphical display 
-	options.add_argument('--headless') 			
+    if(email==None):
+        email = EMAIL
+        print('email = {}'.format(EMAIL))
+    if(password==None):
+        password = PASSWORD
+    	print('password = {}'.format(PASSWORD))
+    options = Options()
+    # not graphical display
+    options.add_argument('--headless')
 
-	# start new session
-	# driver = webdriver.Firefox(options=options)
-	driver = webdriver.Firefox()
+    # start new session
+    # driver = webdriver.Firefox(options=options)
+    driver = webdriver.Firefox()
+    try:
+    	#raw_input()
+    	# navigate to url
+    	driver.get(url)
+    	#raw_input()
+    	# add your email & password
+    	driver.find_element_by_name('email').send_keys(email)
+    	#raw_input()
+    	driver.find_element_by_name('password').send_keys(password)
+    	#raw_input()
+    	# clock on login button
+    	driver.find_element_by_name('action').click()
 
-	# navigate to url
-	driver.get(url)
+    	time.sleep(1)
 
-	# add your email & password
-	driver.find_element_by_name('email').send_keys(email)
-	driver.find_element_by_name('password').send_keys(password)
+    	cookie_list = driver.get_cookies()
+    	cookie_dict = {}
+        cookies_txt = open('cookies.txt','w')
+        for cookie in cookie_list:
+            cookie_txt = ''
+            for key in cookie:
+                print(key)
+                cookie_txt += str(cookie[key])+'\t'
+            cookie_txt += '\n'
+            cookies_txt.write(str(cookie_txt))
+            # print(cookie_txt)
+    		# print cookie
+        cookies_txt.close()
+    	# print('\n\n{}'.format(cookie_list))
 
-	# clock on login button
-	driver.find_element_by_name('action').click()
+    	raw_input()
 
-	# get cokies once you logged in
-	pickle.dump(driver.get_cookies(),open("cookies.pkl","wb"))
+    	# get cokies once you logged in
+    	pickle.dump(driver.get_cookies(),open("cookies.pkl","wb"))
 
-	# dump cookies into a .pkl file (format doesn't matter)
-	cookies = pickle.load(open("cookies.pkl", "rb"))
-	# write cookies to a .txt file in utf-8 format
-	cookies_txt = open('cookies.txt','w')
-	for cookie in cookies:
-	    cookies_txt.write(str(cookie)+'\n')
-	cookies_txt.close()
+    	# dump cookies into a .pkl file (format doesn't matter)
+    	cookies = pickle.load(open("cookies.pkl", "rb"))
+    	# write cookies to a .txt file in utf-8 format
+    	cookies_txt = open('cookies.txt','w')
+    	for cookie in cookies:
+    	    cookies_txt.write(str(cookie)+'\n')
+    	cookies_txt.close()
 
-	# print current page content
-	# print(driver.page_source.encode('utf-8'))
-
-	time.sleep(1)
-
-	# exit driver session
-	driver.quit()
+    	time.sleep(1)
+    except Exception as ex:
+        print(ex)
+    	# exit driver session
+    	driver.quit()
 
 def main():
 	get_cookies()
