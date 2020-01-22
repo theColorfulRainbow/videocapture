5216752a8611ba9b3121a0a81fc3659a206bdd9b
James Hanratty <s1645821@contador.inf.ed.ac.uk>
2020-01-22 18:23:51 +0000

    vscode test

--- Segmentation/config.py
+++ Segmentation/config.py
@@ -8,10 +8,6 @@ lecture_downloading_dir = os.path.join(root_dir,"LectureDownloading")
 information_dir = os.path.join(root_dir,"Information")
 kaltura_dir = os.path.join(root_dir,"Kaltura")
 
-
-
-
-
 # INFORMATION FILES
 VIDEO_DIRECTORY = os.path.join(information_dir,"Videos/segmented_videos")
 COURSE_CSV_FILE = os.path.join(information_dir, "Docs/subject.csv")
