8991ec56415a6dc09343e06671e03be9fb66b415
James Hanratty <s1645821@contador.inf.ed.ac.uk>
2020-01-22 18:25:31 +0000

    hrtesrsgresger

--- Segmentation/config.py
+++ Segmentation/config.py
@@ -7,7 +7,17 @@ video_segmenting_dir = os.path.join(root_dir,"VideoSegmenting")
 lecture_downloading_dir = os.path.join(root_dir,"LectureDownloading")
 information_dir = os.path.join(root_dir,"Information")
 kaltura_dir = os.path.join(root_dir,"Kaltura")
+       
 
+
+
+
+
+
+
+
+
+       
 # INFORMATION FILES
 VIDEO_DIRECTORY = os.path.join(information_dir,"Videos/segmented_videos")
 COURSE_CSV_FILE = os.path.join(information_dir, "Docs/subject.csv")
