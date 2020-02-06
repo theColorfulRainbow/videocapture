1ba8fe5bcbe2e649ec4baa00bffb308c6a6254b7
James Hanratty <s1645821@bobet.inf.ed.ac.uk>
2020-02-06 14:35:03 +0000

    working version

--- Segmentation/VideoSegmenting/Segmentor.py
+++ Segmentation/VideoSegmenting/Segmentor.py
@@ -10,8 +10,6 @@ from threading import Lock
 import numpy as np
 import argparse
 import Sub_Clip
-# import myLogger
-# import logging
 import imutils
 import random
 import time
@@ -21,12 +19,10 @@ import os
 import re
 
 from PIL import Image, ImageFont, ImageDraw
-# from pytesseract import image_to_string
 from ExtractIDData import ExtractIDDataQR
 from Verifier import CourseCodeVerifier, TopicVerifier
 import datetime
 import logging
-# import MasterEnvironemnt.Logger 
 
 
 class Segmentor(object):
@@ -34,19 +30,12 @@ class Segmentor(object):
     Nr_Threads = 1
     logger = logging.getLogger("Logger")
 
-
     def __init__(self, video, threshold_frame, threshold_frame_timeout):
         # self.logger.setLevel(logging.DEBUG)                  # change level depending on what you want to see
         self.logger.info("starting video file thread...")
-
         # replace with something else, like reading a csv file
         self.COURSE_ID_LIST = ['Signals and Communications 2','Signals and Communications 3','Digital Signal Analysis 4','Software Design and Modelling']
-
         self._initialise_variables(video, threshold_frame, threshold_frame_timeout)
-        # start reading frames
-        #self.initialise_reading_frames(self.video_dir, self.id_extractor)
-        # start extracting data from frames
-        #self.initialise_extracting_data(self.fvrf, self.id_extractor)
 
 
     def _initialise_variables(self, video, frame_threshold, threshold_frame_timeout):
@@ -94,41 +83,6 @@ class Segmentor(object):
             self.fvrd.append(FileVideoReadData(fvrf, i, self, id_extractor).start())
 
         self.logger.info("Initialised Extractors Successfully")
-
-    # def update_text_dictionary(self, data, frame_number, frame):
-    #     self.logger.debug("Checking data: ({}), frame_number: {}".format(data,frame_number))
-    #     # use lock so only one FVR thread can update dictionary at a time
-    #     # need to change how we check if data is valid
-    #     self.lock.acquire()
-    #     try:
-    #         # we are checking that the data we have is what we expect
-    #         if (not (self.verifier.verify(data))):
-    #             # self.logger.debug("Data not verified")
-    #             return
-    #         key_data = self.verifier.get_data_index(data)
-    #         #check if topic number already in dictionary, if not then add it
-    #         if (key_data in self.dictionary_frame_data):
-    #             self.logger.debug("Key data ({}) already in frame data dictionary".format(key_data))
-    #             saved_frame_number = self.dictionary_frame_data[key_data]
-    #             #if (saved_frame_number < frame_number):
-    #                 # need to add conditional so that it if we see QR code 10 mins after previously seen, we ignore it
-    #             if ( (saved_frame_number < frame_number) and (frame_number - saved_frame_number < self.THRESHOLD_FRAME_TIMEOUT) ):
-    #                 self.dictionary_frame_data[key_data] = frame_number
-    #                 # check if any topics we have saved in this dict that are bigger than the current topic number. if so then delete them
-    #                 self._remove_higher_topics(self.dictionary_frame_data, key_data)
-                    
-    #                 self.logger.debug("Updated frame data ({}) dictionary with new index {} vs old index {}".format(key_data, frame_number, saved_frame_number))
-    #         else:
-    #             self.logger.info("Key data ({}:{}) not in frame data dictionary, adding entry now...".format(key_data,frame_number))
-    #             self.dictionary_frame_data[key_data] = frame_number
-    #             # enter 
-    #             self.dictionary_start_frame_data[key_data] = frame_number
-
-    #     # only one thread can execute code there
-    #     finally:
-    #         self.lock.release() 
-
-    #     self.logger.debug("Dictionary:\n{}\n".format(self.dictionary_frame_data))
     
     def update_text_dictionary(self, data, frame_number, frame):
         self.logger.debug("Checking data: ({}), frame_number: {}".format(data,frame_number))
@@ -154,9 +108,9 @@ class Segmentor(object):
                 self.logger.info("Key data ({}) has changed from {} -> {}".format(key_data, last_topic_seen, key_data))
                 # we only care about adding valid data to the dictionary
                 if (self.verifier.verify(last_frame_data)):
-                    self.logger.info("Data ({}) is valid, going to check the topic seen array to see if valid QR code seen".format(last_frame_data))
                     self.dictionary_frame_data = _add_topic_frame_values(self.dictionary_frame_data,  last_topic_seen, frame_number_topic_started_at, frame_number_topic_current_at, frame_number_continous_topic_seen,self.THRESHOLD_FRAME_CONTINUOUS, self.THRESHOLD_FRAME_TIMEOUT)
-                self.logger.info("Data ({}) is invalid not updating frame data dictionary".format(data))
+                    self.logger.info("Data ({}) is valid, going to check the topic seen array to see if valid QR code seen".format(last_frame_data))
+                #self.logger.info("Data ({}) is invalid not updating frame data dictionary".format(data))
                 # update the new values for self.current_topic_seen
                 self.current_topic_seen[0] = key_data
                 self.current_topic_seen[1] = frame_number
@@ -202,19 +156,6 @@ class Segmentor(object):
                 break
 
         self.logger.info("All reader threads stopped, final thread read {}".format(self.fvrf.counter))
-        
-    # returns the dictionary for topic numbers which appear for longer than THRESHOLD_FRAME (4s)
-    # def _get_valid_threshold_frame_dictionary(self, dictionary_frame_start, dictionary_frame_end):
-    #     self.logger.info("Creating valid threshold dictionary")
-    #     for topic_number, frame_start in dictionary_frame_start.items():
-    #         # should exist but maybe error during debugging since frame_end_dict not properly updated
-    #         frame_end = dictionary_frame_end[topic_number]
-    #         if (frame_end < frame_start + self.THRESHOLD_FRAME):
-    #             # remove topic number from dictionary
-    #             self.logger.info("Topic {} seen first at frame {} and last seen at frame {}, the difference is below threshold {}. Removing!".format(topic_number, frame_start, frame_end, self.THRESHOLD_FRAME))
-    #             del dictionary_frame_end[topic_number]
-    #     return dictionary_frame_end
-
 
     # ends the Segmentation
     def stop(self):
@@ -248,7 +189,7 @@ def _add_topic_frame_values(dictionary, last_topic_seen, frame_number_topic_star
             dictionary_topic_stored_frame = frame_data_dictionary[last_topic_seen]
             # check if first time we started seeing this data is within 10s of last seen it before
             if (frame_number_topic_started_at - dictionary_topic_stored_frame < THRESHOLD_FRAME_TIMEOUT):
-                logger_test.info("Topic ({}) is within time out, adding!".format(last_topic_seen))
+                logger_test.info("Topic ({}) is within time-out, adding!".format(last_topic_seen))
                 # YES it is within 10s therefore add it to the dictionary
                 frame_data_dictionary[last_topic_seen] = frame_number_topic_current_at
                 logger_test.debug("Frame Dictionary before removing higher topics: {}".format(frame_data_dictionary))
@@ -256,7 +197,7 @@ def _add_topic_frame_values(dictionary, last_topic_seen, frame_number_topic_star
                 frame_data_dictionary = _remove_higher_topics(frame_data_dictionary,last_topic_seen)
                 logger_test.debug("Frame Dictionary after removing higher topics: {}".format(frame_data_dictionary))
             else:
-                logger_test.debug("Topic is not within timeout, not adding")
+                logger_test.info("Topic ({}) is not within timeout, not adding".format(last_topic_seen))
                 # no it is not within 10s therefor do not add it
                 pass
         # it has not been seen in dictionary,
@@ -265,13 +206,15 @@ def _add_topic_frame_values(dictionary, last_topic_seen, frame_number_topic_star
             logger_test.debug("Topic has not been seen in dictionary")
             # check if the first time we seen this and last time is greater than continuous threshold
             if (frame_number_topic_current_at - frame_number_topic_started_at > THRESHOLD_FRAME_CONTINUOUS):
-                logger_test.debug("Topic is successfully continuous, adding it")
+                logger_test.info("Topic ({}) is successfully continuous, adding it".format(last_topic_seen))
                 # add it to the dictionary
                 frame_data_dictionary[last_topic_seen] = frame_number_topic_current_at
             else:
                 # since it is not more than the continous threshold ignore it
-                logger_test.debug("Topic is unsuccessfully continuous, not adding it")
+                logger_test.info("Topic ({}) is unsuccessfully continuous, not adding it".format(last_topic_seen))
                 pass
+    else:
+        logger_test.info("Topic ({}) is unsuccessfully continuous, not adding it".format(last_topic_seen))
     #logger_test.debug("Topic is unsuccessfully continuous, not adding it")
     return frame_data_dictionary
 
